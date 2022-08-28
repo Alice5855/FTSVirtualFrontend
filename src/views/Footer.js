1e8fa0a868f1f79cd104a9582135532127e8b2e1
Alice5855 <105841246+Alice5855@users.noreply.github.com>
2022-08-28 10:49:32 +0900

    Footer is done

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
                     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                         <div className="col-md-4 d-flex align-items-center">
                         <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                            <svg className="bi" width="30" height="24"></svg>
+                            <img src="Justlogo.png" alt="logo" width='45px'/>
                         </a>
                         <span className="mb-3 mb-md-0 text-muted">&copy; 2022 Virtual Agency, Inc</span>
                         </div>
